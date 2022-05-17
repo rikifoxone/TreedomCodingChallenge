e435594ecd30c06ec6520bf0d65b0486c1d89c5f
riki <riccardo.volpone@gmail.com>
2022-05-17 17:08:31 +0200

    rename accessToken to token

--- react-typescript/src/services/auth.service.ts
+++ react-typescript/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const  AUTH_API_URL  = process.env.REACT_APP_AUTH_API_URL;
-//const API_URL = "http://localhost:8080/api/auth/";
+
 
 class AuthService {
   login(email: string, password: string) {
