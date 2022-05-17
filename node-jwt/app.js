7c1f7c2391c1ebae2a799a238ac03bcb3bb660b4
riki <riccardo.volpone@gmail.com>
2022-05-17 16:53:39 +0200

    update Allow headers

--- node-jwt/app.js
+++ node-jwt/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token");
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
